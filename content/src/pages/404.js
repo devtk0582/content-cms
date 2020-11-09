e5437601a4b744ce25ca009d5bc6b1acd5ebed92
Ke Tang <jiushizhutk@gmail.com>
2020-11-09 16:02:43 -0500

    initial setup

---
+++ content/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function error404() {
+  return (
+    <div>
+      <h1>This is 404 error page</h1>
+    </div>
+  )
+}
---
+++ content/src/pages/news.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import styles from '../components/news.module.css'
+const news = () => {
+    return (
+        <Layout>
+          <h1 className={styles.page}>this is our news page</h1>
+        </Layout>
+    )
+}
+
+export default news
